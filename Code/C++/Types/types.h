a2fd99f322776314cbda4a279726405fb1ed531f
Alex-Wasowicz <107991793+Alex-Wasowicz@users.noreply.github.com>
2022-06-22 12:47:06 +0200

    Add files via upload

--- Code/C++/Types/types.h
+++ Code/C++/Types/types.h
@@ -1,1 +1,20 @@
 
+#ifndef TYPES_H_
+#define TYPES_H_
+//name follow rule: https://google.github.io/styleguide/cppguide.html#The__define_Guard
+
+//temporary defined
+#define type typename
+#define name namespace
+#define empty {}
+
+
+
+
+
+
+#undef type
+#undef name
+#undef empty
+
+#endif
