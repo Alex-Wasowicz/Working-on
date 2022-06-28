26494c9a8621f0d2d3601cc1d4dd2c0934135110
Alex-Wasowicz <107991793+Alex-Wasowicz@users.noreply.github.com>
2022-06-28 09:56:11 +0200

    Update numbers.cpp

--- Code/C++/Types/numbers.cpp
+++ Code/C++/Types/numbers.cpp
@@ -1,46 +1,42 @@
 
 #include <climits>
 
+//temporary defined
+#define type typename
+#define name namespace
+
 struct Void { Void() = delete; };
+template <bool constrain> concept tif = constrain; //template_if "tif"
+template <type...> struct Glue;
 
-template <bool constrain>
-concept tif = constrain; //template_if
+struct  int_8; //sizes in at least units!
+struct uint_8;
+struct  int_16;
+struct uint_16;
+struct  int_32;
+struct uint_32;
+struct  int_64;
+struct uint_64;
+struct  int_128;
+struct uint_128;
+template <type> struct int_from;
+template <uint_16 size> struct int_x;
+template <type, type> struct int_glued;
 
-template <type a, type b>
-struct And
+struct float_16; //half
+struct float_32; //float
+struct float_64; //double
+struct float_128;//quadruple
+struct base_30;  //from 0 to 1 in 5 digits of base 30 (30=0b1'1110=2*3*5) 32 bits
+template <uint_16 point, type size> struct fixed;
+template <uint_16 exponent, uint_16 mantissa> struct float_x;
+
+template <type base> struct int_from
 {
-    a left;
-    b right;
-
-    And(): left(), right() {}
-    And(const a& left_, const b& right_): left(left_), right(right_) {}
+    base;
 };
 
-template <type T>
-struct int_x
-{
-    T data;
-
-    inline int_x() = default;
-    inline int_x(const int_x<T>&) = default;
-};
+#undef type
+#undef name
 
 
-
-struct int_8 : int_x
-<
-#if CHAR_BIT >= 8
-    signed char
-#else
-    signed short int
-#endif
->{};
-
-struct uint_8 : int_x
-<
-#if CHAR_BIT >= 8
-    unsigned char
-#else
-    unsigned short int
-#endif
->{};
